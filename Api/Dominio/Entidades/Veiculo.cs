2bbd820f149cbdbcc280119cd4dee36608182350
Eduardo Silva <eduardoalsilva45@gmail.com>
2025-08-20 15:43:54 -0300

    Adicionado testes de Entidade, ServicoMock e VeiculoServicoTest

--- Api/Dominio/Entidades/Veiculo.cs
+++ Api/Dominio/Entidades/Veiculo.cs
@@ -8,14 +8,16 @@ public class Veiculo
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; } = default!;
+    
     [Required]
     [StringLength(150)]
     public string Nome { get; set; } = default!;
+
     [Required]
     [StringLength(100)]
     public string Marca { get; set; } = default!;
-    [Required]
 
+    [Required]
     public int Ano { get; set; } = default!;
 
 }
